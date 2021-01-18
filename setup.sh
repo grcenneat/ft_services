e546bd7c37d1a872d5c1c8ffd0000cc9f29a9be4
hysimok <jeonghy95@gmail.com>
2021-01-18 17:34:33 +0900

    create nginx docker file

--- setup.sh
+++ setup.sh
@@ -5,3 +5,8 @@ eval $(minikube -p minikube docker-env)
 MINIKUBE_IP=$(minikube ip)
 minikube addons enable metallb
 kubectl apply -f ./srcs/metallb/metallb.yaml
+
+# nginx
+cd ./srcs/nginx
+echo "nginx image build"
+docker build -t nginx:latest .
