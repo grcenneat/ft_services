e05b6e99b0a84a7cff655387e1c31b590f059689
hysimok <jeonghy95@gmail.com>
2021-01-27 00:32:18 +0900

    fix reverse proxy (phpmyadmin login bug)

--- setup.sh
+++ setup.sh
@@ -11,13 +11,13 @@ kubectl apply -f ./srcs/metallb/metallb.yaml
 cd ./srcs/nginx
 sed "s/MINIKUBE_IP/$MINIKUBE_IP/g" ./nginx-config.yaml > ./nginx.yaml
 echo "\033[32mnginx image build\033[0m"
-docker build -t nginx:latest .
+docker build -t nginx:latest .	> /dev/null
 kubectl apply -f ./nginx.yaml
 
 # mysql
 cd ../mysql
 echo "\033[32mmysql image build\033[0m"
-docker build -t mysql:latest .
+docker build -t mysql:latest .	> /dev/null
 echo "\033[36mmysql deployment\033[0m"
 kubectl apply -f mysql.yaml
 
@@ -25,7 +25,7 @@ kubectl apply -f mysql.yaml
 cd ../phpmyadmin
 sed "s/MINIKUBE_IP/$MINIKUBE_IP/g" ./phpmyadmin-config.yaml > ./phpmyadmin.yaml
 echo "\033[32mphpmyadmin image build\033[0m"
-docker build -t phpmyadmin:latest .
+docker build -t phpmyadmin:latest .	> /dev/null
 echo "\033[36mphpmyadmin deployment\033[0m"
 kubectl apply -f phpmyadmin.yaml
 
@@ -33,6 +33,6 @@ kubectl apply -f phpmyadmin.yaml
 cd ../wordpress
 sed "s/MINIKUBE_IP/$MINIKUBE_IP/g" ./wordpress-config.yaml > ./wordpress.yaml
 echo "\033[32mwordpress image build\033[0m"
-docker build -t wordpress:latest .
+docker build -t wordpress:latest .	> /dev/null
 echo "\033[36mwordpress deployment\033[0m"
 kubectl apply -f wordpress.yaml
--- srcs/phpmyadmin/config.inc.php
+++ srcs/phpmyadmin/config.inc.php
@@ -31,7 +31,7 @@ $cfg['Servers'][$i]['auth_type'] = 'cookie';
 $cfg['Servers'][$i]['host'] = 'mysql';
 $cfg['Servers'][$i]['compress'] = false;
 $cfg['Servers'][$i]['AllowNoPassword'] = false;
-
+$cfg['PmaAbsoluteUri'] = 'phpmyadmin:5000';
 /**
  * phpMyAdmin configuration storage settings.
  */
